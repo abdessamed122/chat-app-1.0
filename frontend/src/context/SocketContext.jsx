0dd97124ca36f7ce8e6be36e683fd54e7af3512b
abdessamed122 <159251046+abdessamed122@users.noreply.github.com>
2024-08-02 16:50:17 +0200

    Update SocketContext.jsx new

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 	useEffect(() => {
 		if (authUser) {
 			// const socket = io("https://chat-app-yt.onrender.com", {
-                const socket = io("http://localhost:5000", {
+                const socket = io("https://chat-app-1-0-6say.onrender.com", {
 
 				query: {
 					userId: authUser._id,
