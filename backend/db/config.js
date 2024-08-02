e9812cf9bd55af3c5330975f47e9b83c1df570da
abdessamed122 <159251046+abdessamed122@users.noreply.github.com>
2024-08-02 12:51:02 +0200

    Add files via upload

---
+++ backend/db/config.js
@@ -0,0 +1,3 @@
+export const PORT=5000
+export const mongoDB_URL='mongodb+srv://samad:samad082003@cluster0.aktbhqy.mongodb.net/chat-app-dbnew?retryWrites=true&w=majority&appName=Cluster0'
+export const JWT_SECRET = 'tWd+DTDwaUx8bT/a41mLtkpIppttzJqfq0xWb4Nu20U='
