c348373127f9282dfec43c4e61b3584c35a682df
abdessamed122 <159251046+abdessamed122@users.noreply.github.com>
2024-08-31 22:34:49 +0200

    Update MessageContainer.jsx
    no ads

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -20,7 +20,6 @@ const MessageContainer = () => {
 			) : (
 				<>
 					{/* Header */}
-					{<meta name="google-adsense-account" content="ca-pub-3037805956187152">}
 					<div className='bg-slate-500 px-4 py-2 mb-2'>
 					<span className='label-text'>To:</span>{" "}
 					<span className='text-gray-900 font-bold'>{selectedConversation.fullname}</span>
@@ -57,7 +56,6 @@ const NoChatSelected = () => {
 // 			<>
 // 				{/* Header */}
 // 				<div className='bg-slate-500 px-4 py-2 mb-2'>
-	{<meta name="google-adsense-account" content="ca-pub-3037805956187152">}
 // 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
 // 				</div>
 
